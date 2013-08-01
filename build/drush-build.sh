02a8885a68209e7c960b40d98fe397113529b5b9
Dougie Dobrzynski <doug@promethost.com>
2013-08-01 15:42:18 -0500

    Updating drush build script and adding mods to disable list.

--- build/drush-build.sh
+++ build/drush-build.sh
@@ -13,15 +13,15 @@ drush="drush $drush_flags"
 
 build_path=$(dirname "$0")
 
-$drush si --account-pass='drupaladm1n' --db-url=mysql://melcoDBA:melcoPASS@localhost/melcoDB --sites-subdir=default -y &&
-$drush sqlc --db-url=mysql://melcoDBA:melcoPASS@localhost/melcoDB < /vagrant/build/ref_db/melco_db.sql -vd &&
+$drush si --account-pass='drupaladm1n' --db-url=mysql://dana-trainingDBA:dana-trainingPASS@localhost/dana-trainingDB --sites-subdir=default -y &&
+# $drush sqlc --db-url=mysql://dana-trainingDBA:dana-trainingPASS@localhost/dana-trainingDB < /vagrant/build/ref_db/dana-training_db.sql -vd &&
 $drush updb -y &&
 $drush uli &&
 $drush upwd --password="drupaladm1n" admin &&
 chmod 775 /var/drupals/melco/www/sites/default &&
 chmod 775 -R /var/drupals/melco/www/sites/default/files &&
-rm -rf /var/drupals/melco/www/sites/default/files &&
-tar -zxvf $build_path/ref_db/melco_files.tar.gz -C /var/drupals/melco/www/sites/default/ &&
+# rm -rf /var/drupals/melco/www/sites/default/files &&
+# tar -zxvf $build_path/ref_db/melco_files.tar.gz -C /var/drupals/melco/www/sites/default/ &&
 $drush dis $(cat $build_path/mods_purge | tr '\n' ' ') -y &&
 $drush pm-uninstall $(cat $build_path/mods_purge | tr '\n' ' ') -y &&
 $drush en $(cat $build_path/mods_enabled | tr '\n' ' ') -y &&
